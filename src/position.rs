14e3c63275c8c37192cdc9d1cdda6442981ba5ae
James Daly <jamsiedaly@gmail.com>
2021-09-01 16:24:19 +0200

    Now renders map

---
+++ src/position.rs
@@ -0,0 +1,11 @@
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Position {
+    pub x: i32,
+    pub y: i32
+}
+
+impl Position {
+    pub fn new(x: i32, y: i32) -> Position {
+        return Position { x, y };
+    }
+}
